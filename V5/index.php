fbaa5a0dfe723674fa1ed4973981386ff40dc930
AlexU1302 <62549806+AlexU1302@users.noreply.github.com>
2020-04-22 18:42:07 +0200

    Rudimentäre Startseite mit eigenem Stylesheet ergänzt

--- V5/index.php
+++ V5/index.php
@@ -1,24 +1,30 @@
 <?php
-include 'includes/class-autoload.inc.php';
+// PHP-Funktion, damit über mehrere Seiten hinweg
+// eine Session/Sitzung ermöglicht wird.
+// Wir benötigen Sie auf allen unseren Seiten
+session_start();
+
 ?>
+<html>
+   <head>
+     <title>Loginformular</title>
+     <link rel="stylesheet" href="src/css/startseite.css" type="text/css"></link>
+   </head>
+<body>
 
-<!DOCTYPE html> <!-- Zeigt Browser/Texteditor, dass es sich um html handelt -->
-
-<html> <!-- Der "Root-Tag" umschließt das gesamte Dokument, sollte aus formalen Gründen da sein -->
-
-
-<head> <!-- Der "Head" gibt Metadaten zur Webseite an -->
-  <title>FAL:KE</title> <!-- Der "Titel" des Dokuments. Wird im Browsertab und in ggf. in Google-Ergebnissen angezeigt -->
-  <link href="src/css/style.css" rel="stylesheet" type="text/css"> <!-- Die Verknüpfung zu eurem "Stylesheet", wo ihr das Design festlegt -->
-  <meta charset="utf-8"> <!-- Gibt an, dass alle Buchstaben erlaubt sind (wichtig für ö,ä,ß ...) -->
-</head>
-<body> <!-- Der "Body" enthält das, was die Nutzer wirklich sehen -->
-
-  <a href="php/aufnahme.php">Zur Aufnahme</a>
-
-
-
-
+<div class="container">
+  <img src="src/img/minifalcon.png" class="icon" width=100 height =100>
+  <h1>Login</h1>
+  <form action="inc/login-inc.php" method="POST">
+  <input type="text" name="user" placeholder="Benutzername">
+  <input type="password" name="password" placeholder="Passwort"><br>
+  <button type="submit" name="submit" class="button">Login</button>
+  </form>
+  <a href="php/signup.php">Registrieren (funktioniert noch nicht)</a>
+  <br/>
+  <br/>
+  <a href="php/aufnahme.php">Ohne Registrierung gleich weiter zur Aufnahme</a>
+</div>
 
 </body>
 </html>
---
+++ V5/php/signup.php
@@ -0,0 +1,16 @@
+<html>
+   <head>
+     <title>Registrieren</title>
+   </head>
+<body>
+<h1>Registrierungsformular</h1>
+<form action="inc/signup-inc.php" method="POST">
+   <input type="text" name="firstname" placeholder="Vorname"><br>
+   <input type="text" name="lastname" placeholder="Nachname"><br>
+   <input type="text" name="user" placeholder="Benutzername"><br>
+   <input type="password" name="password" placeholder ="Passwort"><br>
+   <button type="submit" name="submit">Registrieren</button>
+</form>
+
+</body>
+</html>
